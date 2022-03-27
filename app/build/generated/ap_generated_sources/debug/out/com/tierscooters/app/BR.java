0cf032f5ab69837452d28fbb46a35e1e7389202b
imankhdrpst <imankhdprst@gmail.com>
2022-03-28 00:36:35 +0430

    Delete app/build directory

--- app/build/generated/ap_generated_sources/debug/out/com/tierscooters/app/BR.java
+++
@@ -1,5 +0,0 @@
-package com.tierscooters.app;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/source/dataBinding/trigger/debug/com/tierscooters/app/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.tierscooters.app;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
